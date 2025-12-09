012588b35e4a35d7e4617b6b03ecac2a38128934
AsharKhursheed <asharkhursheed26@gmail.com>
2025-12-10 03:26:48 +0500

    asa

--- app/Http/Controllers/Api/V1/Admin/Adminsubscriptioncontroller.php
+++ app/Http/Controllers/Api/V1/Admin/Adminsubscriptioncontroller.php
@@ -13,7 +13,7 @@ use Illuminate\Http\Request;
 class AdminSubscriptionController extends Controller
 {
     /**
-     * Get all subscription planss
+     * Get all subscription plansss
      */
     public function plans(Request $request)
     {
